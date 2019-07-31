3a8b12eca899cb2f6a1ee8eed35a00fec408cd64
Lewis Lu <lewis.lu.fs@icloud.com>
2019-07-31 17:46:27 +0800

    修复：百度统计错误的问题

--- assets/js/baidu-hm.js
+++ assets/js/baidu-hm.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?{{ site.3rd.baidu-hm }}";
+    hm.src = "https://hm.baidu.com/hm.js?2ba9b583831274c64c2e68b7065e9bcb";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
